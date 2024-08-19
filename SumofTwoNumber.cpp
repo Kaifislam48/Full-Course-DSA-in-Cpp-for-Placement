6003c91f1757391cb58f5cc04d5fd65a15792ed0
Kaif Islam <117916990+Kaifislam48@users.noreply.github.com>
2024-08-19 23:29:12 +0530

    Add files via upload

---
+++ SumofTwoNumber.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num1, num2;
+    cout<<"Enter 1st number: ";
+    cin>>num1;
+    cout<<"Enter 2nd number: ";
+    cin>>num2;
+    int sum = num1+num2;
+    cout<<"Sum of Two number is "<<sum<<endl;
+}
