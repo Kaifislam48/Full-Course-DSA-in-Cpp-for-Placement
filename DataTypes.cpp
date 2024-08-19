5e60bc5e6de19be3d3fc8d58848247163dea0fda
Kaif Islam <117916990+Kaifislam48@users.noreply.github.com>
2024-08-19 23:27:52 +0530

    Add files via upload

---
+++ DataTypes.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int age = 5;
+    char grade = 'A';
+    bool isAdult = true;
+    float cgpa = 9.2;
+    cout<<age<<" "<<grade<<" "<<isAdult<<" "<<cgpa<<endl;
+    cout<<"Size of Int: "<<sizeof(int)<<endl;
+    cout<<"Size of Char: "<<sizeof(char)<<endl;
+    cout<<"Size of Boolean: "<<sizeof(bool)<<endl;
+    cout<<"Size of Float: "<<sizeof(float)<<endl;
+}
