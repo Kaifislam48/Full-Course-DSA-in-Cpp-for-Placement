28dd43648d35503a1693e45c2c7df6acb085b273
Kaif Islam <117916990+Kaifislam48@users.noreply.github.com>
2024-08-20 23:05:17 +0530

    Add files via upload

---
+++ ConstantsVariable.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    const float PI = 3.14;      //Value Initialization is most important 
+
+    /*const float PI;  //not allowed
+    PI = 3.14;*/
+    
+    /*PI = 4.6;   //Cannot change constant value after one time initilize;*/
+    cout<<PI;
+}
