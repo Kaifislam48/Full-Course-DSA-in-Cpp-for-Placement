b2f9646ccd3aef6fcdf104c17b8997c3d2ffe31e
Kaif Islam <117916990+Kaifislam48@users.noreply.github.com>
2024-09-02 11:25:33 +0530

    Add files via upload

---
+++ LoopQ2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter number: ";
+    cin >> n;
+
+//Print 1 to n Natural Number
+    for(int i=1; i<=n; i++){
+        cout << i << " ";
+    }
+}
